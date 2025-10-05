1c09eb05574bc058b4e48a636ff2c95aeb00805d
Ian <me@ian.app>
2025-10-05 18:17:17 +0800

    Update Firebase project settings

--- launcher/app/src/main/java/asia/chumbaka/kayam/launcher/LoginActivity.java
+++ launcher/app/src/main/java/asia/chumbaka/kayam/launcher/LoginActivity.java
@@ -372,7 +372,7 @@ public class LoginActivity extends KitKitLoggerActivity implements OnItemClick,
                             folder.mkdirs();
                         }
                         File[] files = folder.listFiles();
-                        FirebaseStorage storage = FirebaseStorage.getInstance("gs://kayam-school.appspot.com");
+                        FirebaseStorage storage = FirebaseStorage.getInstance("gs://kayam-school-phase-2.appspot.com");
                         StorageReference storageRef = storage.getReference();
 
                         SharedPreferences preferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
