a08ab44fac4d46b4513c8f00d6f5b925d66927ec
Ian <me@ian.app>
2026-01-11 16:48:53 +0800

    Complete literacy matching in BM

--- mainapp/Classes/Managers/LanguageManager.cpp
+++ mainapp/Classes/Managers/LanguageManager.cpp
@@ -264,7 +264,7 @@ void LanguageManager::initLocalizationMap()
     _localizationMapMalay["Error"] = "Ralat";
     _localizationMapMalay["Enter"] = "Masuk";
     _localizationMapMalay["Clear"] = "Kosongkan";
-    _localizationMapMalay["English"] = "Bahasa Melayu";
+    _localizationMapMalay["English"] = "BM";
     _localizationMapMalay["Math"] = "Matematik";
     
     _localizationMapSwahili["Great!"] = "Vizuri!";
